7e6816753a590dedbe0a05a52ec7d55f6f8f9eea
NikitaK <nikita.karpets@quantag-it.com>
2019-07-26 02:26:11 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StatisticsComponent } from './components/statistics/statistics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'statistics',
+    component: StatisticsComponent
+  },
+  {
+    path: '',
+    redirectTo: 'statistics',
+    pathMatch: 'full'
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { ChartModule } from 'angular2-chartjs';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ChartModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
