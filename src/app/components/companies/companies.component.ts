402841f01bc825ddb22cebaf576a439dafe569af
NikitaK <nikita.karpets@quantag-it.com>
2019-08-13 13:54:01 +0300

    mobile layout fix

--- src/app/components/companies/companies.component.ts
+++ src/app/components/companies/companies.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CompaniesService } from 'src/app/services/companies.service';
 import { CompanyData } from '../admin/admin-interfaces';
+import { HostListener } from "@angular/core";
+
 
 @Component({
   selector: 'app-companies',
   templateUrl: './companies.component.html',
   styleUrls: ['./companies.component.scss']
 })
+
+
 export class CompaniesComponent implements OnInit {
+  innerWidth: number;
   loading = true;
   companiesList: CompanyData[] = [null, null, null];
 
-  constructor(private service: CompaniesService) { }
-
+  constructor(private service: CompaniesService) {
+    this.getScreenSize();
+  }
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?) {
+    this.innerWidth = window.innerWidth;
+    console.log("TCL: CompaniesComponent -> onResize -> this.innerWidth", this.innerWidth)
+  }
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+    console.log("TCL: CompaniesComponent -> onResize -> this.innerWidth", this.innerWidth)
+  }
   ngOnInit() {
+
     this.service.getCompanies().subscribe(data => {
       data.map((company, index) => {
         this.companiesList.splice(index, 1, company);
