d513b7267d8dedcac6d7fdecc6dc37e27ff12e15
Andrii Horievyi <aignbyf@gmail.com>
2020-05-22 18:02:21 +0300

    Remove vhost

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const vhost = require('vhost');
 
 const app = express();
 const app2 = express();
@@ -12,9 +11,6 @@ app.use(express.static(__dirname + '/dist/scrappersSpace'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/scrappersSpace/index.html'));
 });
-
-// add vhost routing to main app for mail
-app.use(vhost('mail.scrappers-space-frontend.herokuapp.com', app2))
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
